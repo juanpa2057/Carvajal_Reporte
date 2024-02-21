1fb72722d0c4a2227f3f990dc71a168f3c66867a
Juan Ocampo <juan_pablo.ocampo@uao.edu.co>
2024-02-21 10:39:39 -0500

    info
    oka oaka

--- mynewbook/_build/jupyter_execute/conclusiones.py
+++ mynewbook/_build/jupyter_execute/conclusiones.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Conclusiones
+# ## Carvajal
 
-# **Generales:** 
-# - Distribución del consumo de energía por máquina y mes: El análisis muestra que la máquina "Energía Activa Extrusora" tiene el mayor consumo de energía, seguida por "Energía Activa Chiller" y "Energía Activa Espumas". Estas tres máquinas representan la mayor parte del consumo total de energía.
+# **Conclusiones y Hallazgos:** 
+# - Distribución del consumo de energía por máquina y mes: El análisis muestra que la máquina "Energía Activa Extrusora" tiene el mayor consumo kWh de energía, seguida por "Energía Activa Chiller" y "Energía Activa Espumas". Estas tres máquinas representan la mayor parte del consumo kWh total de energía.
 # 
-# - El consumo de energía fue mayor en octubre, seguido por noviembre y agosto. Esto indica que octubre  fue el mes con mayor demanda de energía analizado.
+# - Consumo por tipo de día: El análisis revela que los días jueves y viernes exhiben un mayor consumo de energía, aproximadamente un 3% más alto en comparación con los otros días de la semana. Por otro lado, los fines de semana, particularmente los domingos, muestran una notable disminución del consumo de energía, disminuyendo hasta un 53.2% en comparación con el jueves.
 # 
-# - Consumo por tipo de día: El análisis muestra que los días jueves y viernes tienen un mayor consumo de energía en comparación con los demás días de la semana. Los fines de semana, especialmente los domingos, tienen un consumo más bajo de energía.
+# <div style="text-align:center">
 # 
-# - Consumo horario de la planta por proceso: El gráfico muestra la distribución del consumo de energía en la planta a lo largo del día, desglosado por proceso. Se observa que el consumo varía a lo largo del día para cada proceso, con picos de consumo en diferentes momentos.
+# ![newplot.png](attachment:newplot.png)
 # 
-# - Se observa que el consumo de energía para la extrusora es mayor en el tercer turno donde se evidencia que desde las 22:00 horas hasta las 6:00 horas aumenta. Se recomienda validar con personal de campo las actividades y procesos que se están llevando a cabo durante este período de tiempo.
+# </div>
+# 
+# - Los análisis indican que los viernes y jueves presentan un aumento del 3% en la demanda de energía eléctrica. Siguiendo esta observación y en línea con las conversaciones mantenidas con el equipo de Carvajal, donde se sugiere que la producción se mantiene constante en promedio a lo largo de la semana, podría plantearse la hipótesis de que la productividad del proceso podría disminuir para los otros días de la semana.
 # 
 # 
-# **Sistemas de enfriamiento:**
-# - Mejora de aislamiento térmico de tanques.
+# - Se observa que el consumo de energía para la extrusora es mayor por un 2.83% de consumo en el tercer turno donde se evidencia que desde las 22:00 horas hasta las 6:00 horas aumenta. Se recomienda validar con personal de campo las actividades y procesos que se están llevando a cabo durante este período de tiempo.
 # 
-# - Agua gana aproximadamente 2°C mientras permanece almacenada en el tanque.
+# <div style="text-align:center">
+# 
+# ![estrusora.png](attachment:estrusora.png)
+# 
+# </div>
+# 
+# **Recomendación Sistemas de enfriamiento:**
+# - Mejora de aislamiento térmico de tanques. Agua gana aproximadamente 2°C mientras permanece almacenada en el tanque.
 #  
-# - Incremento del factor de carga del chiller Trane.
+# - Incremento del factor de carga del chiller Trane. Promedio de carga del chiller Trane es de 50%, se propone llevarlo a porcentajes de carga cercanos al 80% hasta antes de encender chillers auxiliares, teniendo en cuenta que el chiller Trane es el más eficientes los equipos disponibles.
 # 
-# - Promedio de carga del chiller Trane es de 50%, se propone llevarlo a porcentajes de carga cercanos al 80% hasta antes de encender chillers auxiliares, teniendo en cuenta que el chiller Trane es el más eficientes los equipos disponibles.
+# - Con el fin de obtener mayor información de los procesos medidos, será necesario ampliar el alcance del proyecto aumentando el número de variables medidas (físicas y eléctricas). Adicional, se deben medir las ratas de producción de las máquinas que están siendo monitoreadas. Esto con el fin de estimar variaciones en la productividad de la máquina (kWh/Unidad producida). De esta manera se podrá analizar la disciplina operativa que se tenga en los procesos, tiempos muertos de producción o de paro del proceso y posibles estimaciones de costos reales que llevaran a tener información precisa sobre cuales son las acciones que se deben seguir para lograr ahorros significativos.
